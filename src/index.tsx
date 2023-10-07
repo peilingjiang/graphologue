247d976ba720d6f2a67e046f422f5e3cf1113d8a
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2023-10-07 12:06:17 -0700

    add electron and compiled app

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserHistory, createHashHistory } from 'history'
 
 import { ChatApp } from './App'
 
 // ! the only css imports in ts/x files
 import './css/index.scss'
 import 'reactflow/dist/style.css'
+import { isElectron } from './electron'
 
 if (process.env.NODE_ENV === 'production') {
   console.log = () => {}
   console.error = () => {}
   console.debug = () => {}
 }
+
+export const history = isElectron()
+  ? createHashHistory()
+  : createBrowserHistory()
 
 /* -------------------------------------------------------------------------- */
 
